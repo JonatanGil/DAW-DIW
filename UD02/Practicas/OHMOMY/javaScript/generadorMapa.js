e7207d65e47b6248b8a5a4ecba3034af0b397ebe
jon Gil <jonyqro@gmail.com>
2019-10-17 20:40:52 +0200

    fila cajas primera terminada falta bajar y ya estaria el juego casi acabado animaciones de lalves pergamins etc

--- UD02/Practicas/OHMOMY/javaScript/generadorMapa.js
+++ UD02/Practicas/OHMOMY/javaScript/generadorMapa.js
@@ -67,227 +67,283 @@ window.onload = function(){
     para.appendChild(t);                                          // Append the text to <p>
     document.getElementById("main").appendChild(para);           // Append <p> to <div> with id="myDIV" */
 
+    terminar();
 
-    for (var filas = 0; filas < mapa[0].length; filas++) {
 
-    for (var columnas = 0; columnas < mapa.length; columnas++) {
-        
 
-        //primero vemos si es una momia o jugador. despues imprimimos camino CajaPosicion si no es.
-        console.log((filas)+" "+momiaY);
-        console.log(columnas+" "+momiaX);
-        if(filas==momiaY && columnas==momiaX ){
-            console.log("entraMomia");
-            mapa[columnas][filas]="M";
-            addDiv(columnas,filas,1,"M");
+    function dibujarMapa(){
 
-        }else{
 
-            if(filas==personajeY && columnas==personajeX){
+        for (var filas = 0; filas < mapa[0].length; filas++) {
 
-                console.log("entraPersonaje");
-                mapa[columnas][filas]="1";
-                addDiv(columnas,filas,1,"P");
-
-            }else{
-
-            //filas que no chocan con las cajas que contienen cofres llaves etc..
-            if(filas==1 | filas==4 | filas==7 | filas==10 | filas==13 | (columnas==4 && filas!=0) | columnas==8 | 
-            //la primera fila en amarillo menos el start 
-            (columnas==0 && filas!=0) | (columnas==12 && filas!=0) | (columnas==16 && filas!=0) | (columnas==20 && filas!=0) ){
-
-                //añadimos 1 al valor de ese div, texto, tal vez cambio por value del atributo del div.
-                mapa[columnas][filas]=1;
-                //los caminos pasamos el 1 para el style
-                addDiv(columnas,filas,1,figura);
+            for (var columnas = 0; columnas < mapa.length; columnas++) {
                 
-            }else{
-
-                if(filas==0){
-                    console.log(columnas+" columna"+" filas"+filas);
-                    if(columnas==8){
-                    //primera fila solo el start
-                    mapa[columnas][filas]="1";
-                    //la primera linea solo marcamos el start 0 para el style vacio
-                    addDiv(columnas,filas,0,figura);
+        
+                //primero vemos si es una momia o jugador. despues imprimimos camino CajaPosicion si no es.
+                console.log((filas)+" "+momiaY);
+                console.log(columnas+" "+momiaX);
+                if(filas==momiaY && columnas==momiaX ){
+                    console.log("entraMomia");
+                    mapa[columnas][filas]="M";
+                    addDiv(columnas,filas,1,"M");
+        
                 }else{
-                    if(columnas==8 && filas==0){
-                        alert("1");
-                        //primera fila solo el start
+        
+                    if(filas==personajeY && columnas==personajeX){
+        
+                        console.log("entraPersonaje");
                         mapa[columnas][filas]="1";
-                        //la primera linea solo marcamos el start 0 para el style vacio
-                        addDiv(columnas,filas,0,figura);
+                        addDiv(columnas,filas,1,"P");
+        
                     }else{
-                        //primera fila solo el start
-                        mapa[columnas][filas]="0";
-                        //la primera linea solo marcamos el start 0 para el style vacio
-                        addDiv(columnas,filas,0,figura);
+        
+                    //filas que no chocan con las cajas que contienen cofres llaves etc..
+                    if(filas==1 | filas==4 | filas==7 | filas==10 | filas==13 | (columnas==4 && filas!=0) | columnas==8 | 
+                    //la primera fila en amarillo menos el start 
+                    (columnas==0 && filas!=0) | (columnas==12 && filas!=0) | (columnas==16 && filas!=0) | (columnas==20 && filas!=0) ){
+        
+                        //añadimos 1 al valor de ese div, texto, tal vez cambio por value del atributo del div.
+                        mapa[columnas][filas]=1;
+                        //los caminos pasamos el 1 para el style
+                        addDiv(columnas,filas,1,figura);
+                        
+                    }else{
+        
+                        if(filas==0){
+                            console.log(columnas+" columna"+" filas"+filas);
+                            if(columnas==8){
+                            //primera fila solo el start
+                            mapa[columnas][filas]="1";
+                            //la primera linea solo marcamos el start 0 para el style vacio
+                            addDiv(columnas,filas,0,figura);
+                        }else{
+                            if(columnas==8 && filas==0){
+                                alert("1");
+                                //primera fila solo el start
+                                mapa[columnas][filas]="1";
+                                //la primera linea solo marcamos el start 0 para el style vacio
+                                addDiv(columnas,filas,0,figura);
+                            }else{
+                                //primera fila solo el start
+                                mapa[columnas][filas]="0";
+                                //la primera linea solo marcamos el start 0 para el style vacio
+                                addDiv(columnas,filas,0,figura);
+                            }
+                           
+                        }
+        
+                        }else{
+        
+                            //añadimos el 2 para el style de la CajaPosicion
+                            mapa[columnas][filas]="2";
+                            //las cajas pasamos el 2 para el style
+                            addDiv(columnas,filas,2,figura);
+        
                     }
-                   
+        
+                    }
+        
                 }
-
-                }else{
-
-                    //añadimos el 2 para el style de la CajaPosicion
-                    mapa[columnas][filas]="2";
-                    //las cajas pasamos el 2 para el style
-                    addDiv(columnas,filas,2,figura);
-
             }
-
+                    /*console.log(mapa[columnas][filas]);
+                    this.console.log(columnas+" "+filas);*/
+        
+                }
+        
             }
-
-        }
-    }
-            /*console.log(mapa[columnas][filas]);
-            this.console.log(columnas+" "+filas);*/
-
-        }
 
     }
 
     function confirmarCuadrado(mapa){
 
         var CajaPosicion = 25;
-        ///sumas al bucle 47 para pasar de la quinta caja a las sextacon los divs* para pintar y saber si esta rodeado
-        var contarCajas = 47;
+        ///bajas a las cajaas de abajo con los divs
+        var saltarFila = 0;
 
         var cuadradoDerecha=false;
-        var cuadradIzquierda=false;
+        var cuadradoIzquierda=false;
         var cuadradoArriba=false;
         var cuadradoAbajo=false;
 
         let posicionDelCuadradoDiv=[43,47,51,55,59,106,110,114,118,122,169,173,177,181,183,232,236,240,244,248];
 
-        for (var i = 0; i < 1; i++) {
+        for (var i = 0; i < 20; i++) {
 
-
-            for (let index = CajaPosicion; index > CajaPosicion-5; index--) {
-
-                var cajas = document.getElementById(index);
-                var valor = cajas.getAttribute("valor");
-
-                console.log(index+"posicion     valor"+valor+"  izquierda");
+            try {
                 
-                if(valor == 1){
-                    cuadradIzquierda=true;
-                }else{
-                    cuadradIzquierda=false;
-                    //es falso salimos false el bolean
-                    console.log("sale");
-                    index=Number.MAX_VALUE;
-                }
-            }
             
+            for (var a = CajaPosicion; a > CajaPosicion-5; a--) {
 
+                var cajas = document.getElementById(a);
+                var valor = cajas.getAttribute("valor");
 
-            //arriba a abajo
+                //console.log(a+"posicion     valor"+valor+"  izquierda");
+                console.log("porque es true"+a);
+                if(valor == 1){
+                    cuadradoIzquierda=true;
+                    console.log("posicion del div: "+a+"      valor==1"+valor+"      numerodelacaja:"+i);
+                }else{
+                    cuadradoIzquierda=false;
+                    //es falso salimos false el bolean
+                    a=Number.MAX_VALUE;
+                }
+                //console.log("index"+a+" i"+i);
+            }
+        
             CajaPosicion=CajaPosicion-4+21;
-            console.log(CajaPosicion+21+21+" cajanormal"+CajaPosicion);
+        } catch (error) {
+            CajaPosicion=CajaPosicion-4+21;
+            cuadradoIzquierda=false;
+    }
 
-            for (var index = CajaPosicion; index <= CajaPosicion+21+21; index=index+21) {
 
-                var cajas = document.getElementById(index);
+    try {
+                
+            
+            //arriba a abajo
+
+            for (var b = CajaPosicion; b <= CajaPosicion+21+21; b=b+21) {
+
+                var cajas = document.getElementById(b);
                 var valor = cajas.getAttribute("valor");
                 
-                console.log(index+"posicion     valor"+valor+"  abajo");
+                //console.log(b+"posicion     valor"+valor+"  abajo");
                 if(valor == 1){
                     cuadradoAbajo=true;
                 }else{
                     cuadradoAbajo=false;
                     //es falso salimos false el bolean
-                    index=Number.MAX_VALUE;
+                    b=Number.MAX_VALUE;
                 }
 
 
             }
 
-
             CajaPosicion=CajaPosicion+21+21;
-            //derecha
-            for (let index = CajaPosicion; index < CajaPosicion+5; index++) {
+        } catch (error) {
+            CajaPosicion=CajaPosicion+21+21;
+            cuadradoAbajo=false;
+    }
 
-                var cajas = document.getElementById(index);
+
+
+
+    try {
+              
+
+            //derecha
+            for (var c = CajaPosicion; c < CajaPosicion+5; c++) {
+
+                var cajas = document.getElementById(c);
                 var valor = cajas.getAttribute("valor");
 
-                console.log(index+"posicion     valor"+valor+"  derecha");
+                //console.log(c+"posicion     valor"+valor+"  derecha");
                 
                 if(valor == 1){
                     cuadradoDerecha=true;
                 }else{
                     cuadradoDerecha=false;
                     //es falso salimos false el bolean
-                    console.log("sale");
-                    index=Number.MAX_VALUE;
+                    c=Number.MAX_VALUE;
                 }
             }
 
-
             CajaPosicion=CajaPosicion+4-21;
+        } catch (error) {
+            CajaPosicion=CajaPosicion+4-21;
+            cuadradoArriba = false;
+    }
 
-            for (let index = CajaPosicion; index > CajaPosicion-21-21; index=index-21) {
 
-                var cajas = document.getElementById(index);
+
+
+    try {
+              
+
+            for (var d = CajaPosicion; d > CajaPosicion-21-21; d=d-21) {
+
+                var cajas = document.getElementById(d);
                 var valor = cajas.getAttribute("valor");
 
-                console.log(index+"posicion     valor"+valor+"  arriba");
+                //console.log(d+"posicion     valor"+valor+"  arriba");
                 if(valor == 1){
                     cuadradoArriba=true;
                 }else{
                     cuadradoArriba=false;
                     //es falso salimos false el bolean
-                    console.log("sale");
-                    index=Number.MAX_VALUE;
+                    d=Number.MAX_VALUE;
                 }
                 
             }
-            
+     
+        } catch (error) {
+            cuadradoArriba=false;
+    }
 
 
 
-            console.log(cuadradIzquierda+"-izquierda");
+            console.log(cuadradoIzquierda+"-izquierda");
             console.log(cuadradoAbajo+"-abajo");
             console.log(cuadradoDerecha+"-derecha");
-            console.log(cuadradoArriba+"-arriba");
+            console.log(cuadradoArriba+"-arriba\n");
+            console.log("*******");
         
-            if(cuadradoDerecha && cuadradIzquierda && cuadradoArriba && cuadradoAbajo){
-                if(cuadradoDescubierto.indexOf(i,0)){
+            if(cuadradoDerecha && cuadradoIzquierda && cuadradoArriba && cuadradoAbajo){
+                    if(cuadradoDescubierto.indexOf(i)){
                     cuadradoDescubierto.push(i);
+                }
+            
             }
-            }
+
+           /* if(i%4==0){
+        }else{
+            CajaPosicion==CajaPosicion+6;
+        }*/
+
+        if(i%4==4){
+            CajaPosicion=CajaPosicion+6;
+        }else{
+            CajaPosicion=CajaPosicion-21-21+4;
+        }
+            //alert(" Posicion de las cajas"+CajaPosicion);
+
+
             cuadradoDerecha=false;
-            cuadradIzquierda=false;
+            cuadradoIzquierda=false;
             cuadradoArriba=false;
             cuadradoAbajo=false;
 
 
-
         }
 
-        console.log(cuadradoDescubierto);
-/*
-        for (let index = 0; index <= 1; index++) {
+        console.log(cuadradoDescubierto+"  mas length de los cuadrados ok"+cuadradoDescubierto.length);
 
         
 
-        //obetenesmoes el div para pintar las caja 3 a la derecgha y abajo repetimos
-            var primeraPosicionDelaCaja = cuadradoDescubierto.shift();
-            var pintarCaja = posicionDelCuadradoDiv[primeraPosicionDelaCaja];
-            console.log(primeraPosicionDelaCaja+"cuadrad a pintar             "+pintarCaja+"numero de la caja prierma a pintar");
-            for (let dosveces = 0; dosveces < 2; dosveces++) {
+            for (let index = 0; index < cuadradoDescubierto.length; index++) {
                 
-                for (let B = pintarCaja; B < pintarCaja+3; B++) {
-                    var CajaPosicion = document.getElementById(B);
+            
+
+                var primeraPosicionDelaCaja = cuadradoDescubierto[index];
+                var pintarCaja = posicionDelCuadradoDiv[primeraPosicionDelaCaja];
+    
+              //  console.log(primeraPosicionDelaCaja+"cuadrad a pintar             "+pintarCaja+"numero de la caja prierma a pintar");
+                  //obetenesmoes el div para pintar las caja 3 a la derecgha y abajo repetimos
+               for (let dosveces = 0; dosveces < 2; dosveces++) {
+                
+                   for (let B = pintarCaja; B < pintarCaja+3; B++) {
+                       var CajaPosicion = document.getElementById(B);
                     
-                    CajaPosicion.classList.add("visibleCaja");
-                    
-                }
-                pintarCaja  +=21;
+                           CajaPosicion.classList.add("visibleCaja");
+
+                 }
+                    pintarCaja  +=21;
             
            
-        }
+                  }
+    
     }
-*/
+
 
 
     }
@@ -301,6 +357,8 @@ window.onload = function(){
             var posicionPersonajeVieja = document.getElementById(posicionDivPersonaje);
 
             posicionPersonajeVieja.classList.add("divCaminado");
+
+            posicionPersonajeVieja.classList.add("divCamino");
 
             posicionPersonajeVieja.setAttribute("valor",1);
 
@@ -341,6 +399,7 @@ window.onload = function(){
         }
         }
         if(movimiento=="rigth"){
+            if((posicionDivPersonaje+1)%21==0) return;
             //sumo una a la columna "Y"
             personajeY-=1;
             //ir hacia la derecha +1
@@ -358,6 +417,7 @@ window.onload = function(){
             }
         }
         if(movimiento=="left"){
+            if(posicionDivPersonaje%21==0) return;
             //sumo una a la fila "Y"
             personajeY-=1;
             //ir hacia a la izquierda -11
@@ -452,4 +512,12 @@ window.onload = function(){
     var classDiv = document.getElementsByClassName("cuadrado");
     classDiv.classList.add("cuadradp");*/
 
+
+    function terminar(){
+        var altura = window.innerHeight-18+"px";
+        
+
+        document.getElementById("pagina").style.height=altura;
+        dibujarMapa();
+}
 };
