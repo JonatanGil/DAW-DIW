3e90754f4daebc09352c15831bfaa93af873f694
jon Gil <jonyqro@gmail.com>
2020-02-11 20:22:30 +0100

    añado pokedez casi terminada falta imprimir

--- EVAL2/pokedex/pokedex/src/App.js
+++ EVAL2/pokedex/pokedex/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+//import logo from './public/logo192.png';
 //const logo = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png';
-
-
-//<React.Frafment>
-//</React.Frafment>
 
 
 class Buscador extends React.Component {
   //input = (<input type='text' onClick={buscarPokemon}></input>);
   constructor(props) {
     super(props);
-    this.state = { value: 'Pokemon a buscar aquí' };
-    this.pokemonsLista = [{}];
-    this.pokemonActual = {};
+    this.state = {
+      value: 'Pokemon a buscar aquí',
+      pokemonActual: {},
+      pokemonsLista: [{}]
+
+    };
 
     //porque hacer llamadas asi
     this.cambiarNombreCuandoCambia = this.cambiarNombreCuandoCambia.bind(this);
@@ -24,119 +24,111 @@ class Buscador extends React.Component {
     this.setState({ value: event.target.value });
   }
 
-  componentDidUpdate() {
-    this._commitAutoSave();
-  }
-  async _commitAutoSave() {
-    const pokemonListaIDS = await fetch('https://pokeapi.co/api/v2/pokemon');
-    const jsonOK = await pokemonListaIDS.json();
-
-    //console.log(jsonOK.results[0].name);
-    //console.log(this.state.value);
-    if (jsonOK.results[0].name == this.state.value) {
-      console.log(jsonOK.results[0].url);
-      const pokemonActualJson = await fetch(jsonOK.results[0].url);
-      this.pokemonActual = await pokemonActualJson.json();
-      console.log(this.pokemonActual);
-
-
-
-    } else {
-      //console.log("id:132 == ditto");
-      const pokemonActualJson = await fetch('https://pokeapi.co/api/v2/pokemon/132');
-      this.pokemonActual = await pokemonActualJson.json();
-
-
-    }
-
-
-  }
-
-
-
-  hacerSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    //function buscarPokemon(this.state.value,this.pokemonsLista);
-    event.preventDefault();
-  }
-
-  vaciarNombre() {
-    this.setState({ value: "" });
-  }
-  /*
-    async componentDidMount() {
-      //carga todos los pokemones(20?? limit) y tenemos el json para cuando haga el submit imprimimos pokemon
-      const pokemonListaIDS = await fetch('https://pokeapi.co/api/v2/pokemon');
-      const jsonOK = await pokemonListaIDS.json();
-      //console.log(jsonOK);
-      this.pokemonsLista = jsonOK;
-      //console.log(this.pokemonsLista);
-  
-      //directamente el object ahi y au console.log(this.pokemonsLista.pokemon);
-  
-    }*/
-
-  render() {
-
-    return (
-      <div className="App-buscador">
-        <form onSubmit={this.hacerSubmit}>
-          <label>
-            Pokemones:
-          <input type="text" value={this.state.value} onChange={this.cambiarNombreCuandoCambia} onClick={this.vaciarNombre} />
-          </label>
-          <input type="submit" value="Buscar" />
-        </form>
-      </div>
-    );
-  }
-}
-
-async function ditto(){
-  let jsonPokemonDefault = await fetch('https://pokeapi.co/api/v2/pokemon/132');
-  let jsonPokemonDefaultJson = jsonPokemonDefault.json();
-  return jsonPokemonDefaultJson;
-}
-
-
-class Pokemon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.pokemonDefaultJson = ditto();
-    console.log(this.pokemonDefaultJson);
-
-    this.nombre = "Ditto";
-    this.type = "poison";
-    this.img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png";
-    this.peso = "40";
-    this.mov = ["imposter", "limber", "mov3"];
-  }
-
   async componentDidMount() {
+    const pokemonListaIDS = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000');
+    const pokemonsListaJson = await pokemonListaIDS.json();
 
-    /*const obetenerJSON = new Buscador();
-    const jsonOK = await obetenerJSON.pokemonsLista;
-    console.log(jsonOK);*/
+
+console.log(pokemonsListaJson);
+    const pok = pokemonsListaJson.results.map(async pokemone => {
+
+        const pokemonActualJson = await fetch(pokemone.url);
+        const pokemonDevolver = await pokemonActualJson.json();
+        return pokemonDevolver;
+
+
+    });
+
+
+    this.setState({
+      pokemonsLista: pok,
+    });
+
+
+}
+
+
+
+
+
+  componentDidUpdate() {
+    //this._commitAutoSave();
+  }
+
+  async _commitAutoSave() {
+
+    console.log("hola");
+
+
 
 
   }
 
-  render() {
-    return (
-      <div className="App-center">
+
+
+
+hacerSubmit(event) {
+  alert('A name was submitted: ' + this.state.value);
+  //function buscarPokemon(this.state.value,this.pokemonsLista);
+  event.preventDefault();
+}
+
+vaciarNombre() {
+  this.setState({ value: "" });
+}
+
+render() {
+  /*
+          <img src={this.img} className="App-logo" alt="logo" />
+          <h3>{this.nombre}</h3>
+          <h3>{this.type}</h3>
+          <h3>{this.peso}</h3>
+          <h3>{this.mov[0]}</h3>
+          <h3>{this.mov[1]}</h3>
+          <h3>{this.mov[2]}</h3>*/
+
+  return (
+    <div className="App-buscador">
+      <form onSubmit={this.hacerSubmit}>
+        <label>
+          Pokemones:
+          <input type="text" value={this.state.value} onChange={this.cambiarNombreCuandoCambia} onClick={this.vaciarNombre} />
+        </label>
+        <input type="submit" value="Buscar" />
+      </form>
+
+    </div>
+  );
+}
+}
+
+//<Pokemon valorPokemon={this.state.pokemonActual}/>
+
+
+function Pokemon({ valorPokemon }) {
+  if (Object.keys(valorPokemon).length == 0) return null;
+  return (
+    <div className="App-center">
+      <img src={valorPokemon.sprites.front_default} className="App-logo" alt="logo" />
+
+    </div>
+  );
+}
+
+
+/*
         <img src={this.img} className="App-logo" alt="logo" />
         <h3>{this.nombre}</h3>
         <h3>{this.type}</h3>
         <h3>{this.peso}</h3>
         <h3>{this.mov[0]}</h3>
         <h3>{this.mov[1]}</h3>
-        <h3>{this.mov[2]}</h3>
-      </div>
-    );
-  }
-}
+        <h3>{this.mov[2]}</h3>*/
 
 //<img src={logo} className="App-logo" alt="logo" />
+
+
+
 function App() {
 
   //let input = new Buscador;
@@ -145,9 +137,6 @@ function App() {
     <div className="App-body">
       <header className="App-header">
         <Buscador />
-        <Pokemon />
-
-
       </header>
     </div>
   );
