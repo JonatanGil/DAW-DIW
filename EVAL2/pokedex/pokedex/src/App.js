4409cba32cbd9a2544f35919b59399802da036cf
jon Gil <jonyqro@gmail.com>
2020-02-12 20:42:00 +0100

    pokedex terminada falta css y q quede bonito

--- EVAL2/pokedex/pokedex/src/App.js
+++ EVAL2/pokedex/pokedex/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+//import Pokemon from './DevolverPokemones.js';
 //import logo from './public/logo192.png';
 //const logo = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png';
 
@@ -9,9 +10,9 @@ class Buscador extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 'Pokemon a buscar aquí',
+      value: "Pokemon a buscar aquí",
       pokemonActual: {},
-      pokemonsLista: [{}]
+      pokemonsLista: [{}],
 
     };
 
@@ -25,91 +26,131 @@ class Buscador extends React.Component {
   }
 
   async componentDidMount() {
+
+
     const pokemonListaIDS = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000');
     const pokemonsListaJson = await pokemonListaIDS.json();
 
-
-console.log(pokemonsListaJson);
     const pok = pokemonsListaJson.results.map(async pokemone => {
+      //obtenemos todos 
+      const pokemonActualJson = await fetch(pokemone.url);
+      const pokemonDevolver = await pokemonActualJson.json();
 
-        const pokemonActualJson = await fetch(pokemone.url);
-        const pokemonDevolver = await pokemonActualJson.json();
-        return pokemonDevolver;
+      return pokemonDevolver;
 
 
     });
 
-
-    this.setState({
-      pokemonsLista: pok,
-    });
-
-
-}
-
+    //promise all obtiene todas las respuestas de las promesas
+    const pokes = Promise.all(pok).then(resp => {
+      console.log(resp);
+      this.setState({
+        pokemonsLista: resp,
+      });
 
 
+    })
 
 
+
+  }
   componentDidUpdate() {
     //this._commitAutoSave();
   }
 
   async _commitAutoSave() {
-
     console.log("hola");
+  }
 
+  hacerSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    //function buscarPokemon(this.state.value,this.pokemonsLista);
+    event.preventDefault();
+  }
 
+  vaciarNombre() {
+    this.setState({ value: "" });
+  }
 
+  render() {
+    /*
+            <img src={this.img} className="App-logo" alt="logo" />
+            <h3>{this.nombre}</h3>
+            <h3>{this.type}</h3>
+            <h3>{this.peso}</h3>
+            <h3>{this.mov[0]}</h3>
+            <h3>{this.mov[1]}</h3>
+            <h3>{this.mov[2]}</h3>*/
 
+    /*        console.log(this.state.pokemonsLista);
+    if (this.state.value.length>2) {
+      
+      this.state.pokemonsLista.forEach(peke => {
+        if (peke.name == this.state.value) {
+          console.log();
+          this.setState({
+            pokemonActual: peke,
+          });
+        }
+      });
+    }*/
+
+    console.log(this.state.value);
+
+    //carga la funcion pokemon undefined y peta ?? antes que el didunmount carga antes el render??
+    if(this.state.pokemonsLista[0].sprites==null){return null}
+
+    return (
+      <div className="App-buscador">
+        <form onSubmit={this.hacerSubmit}>
+          <label>
+            Pokemones:
+          <input type="text" value={this.state.value} onChange={this.cambiarNombreCuandoCambia} onClick={this.vaciarNombre} />
+          </label>
+          <input type="submit" value="Buscar" />
+        </form>
+        <Pokemon valorPokemon={this.state} />
+
+      </div>
+    );
+  }
+ 
+  
+}
+
+function Pokemon({valorPokemon}){
+
+  var pokemonActual;
+  const pokemones = valorPokemon.pokemonsLista;
+  const nombrePokemon = valorPokemon.value;
+
+  //obtenemos el pokemon solo uno demomento
+  pokemones.forEach(peke => {
+    //console.log(peke.name);
+    if(peke.name==nombrePokemon){
+      pokemonActual = peke;
+    }
+  });
+
+  //si el pokemon no existe en la lista imprime el ditto
+  if(pokemonActual==null){
+    //ditto
+    console.log(" pokemones null");
+    pokemonActual = pokemones[131];
   }
 
 
-
-
-hacerSubmit(event) {
-  alert('A name was submitted: ' + this.state.value);
-  //function buscarPokemon(this.state.value,this.pokemonsLista);
-  event.preventDefault();
-}
-
-vaciarNombre() {
-  this.setState({ value: "" });
-}
-
-render() {
-  /*
-          <img src={this.img} className="App-logo" alt="logo" />
-          <h3>{this.nombre}</h3>
-          <h3>{this.type}</h3>
-          <h3>{this.peso}</h3>
-          <h3>{this.mov[0]}</h3>
-          <h3>{this.mov[1]}</h3>
-          <h3>{this.mov[2]}</h3>*/
-
-  return (
-    <div className="App-buscador">
-      <form onSubmit={this.hacerSubmit}>
-        <label>
-          Pokemones:
-          <input type="text" value={this.state.value} onChange={this.cambiarNombreCuandoCambia} onClick={this.vaciarNombre} />
-        </label>
-        <input type="submit" value="Buscar" />
-      </form>
-
-    </div>
-  );
-}
-}
-
-//<Pokemon valorPokemon={this.state.pokemonActual}/>
-
-
-function Pokemon({ valorPokemon }) {
-  if (Object.keys(valorPokemon).length == 0) return null;
+  //if (Object.keys(valorPokemon).length == 0) return null;
   return (
     <div className="App-center">
-      <img src={valorPokemon.sprites.front_default} className="App-logo" alt="logo" />
+      <img src={pokemonActual.sprites.front_default} className="App-logo" alt="logo" />
+        <h3>{pokemonActual.name}</h3>
+        <h3>{pokemonActual.types[0].type.name}</h3>
+        <h3>{pokemonActual.weight}</h3>
+        {pokemonActual.moves.map(move => {   
+          console.log(move)       
+         return ( <h3>{move.move.name}</h3>)
+        })}
 
     </div>
   );
